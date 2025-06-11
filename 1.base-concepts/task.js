0b19684e331021ae58c4d1e1e0ac9ef6780c2a7e
Yar neto <quatro30@gmail.com>
2025-06-11 21:09:37 +0700

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,16 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    return arr;
+  } 
+  else if (d === 0) {
+    arr.push(-b / (2 * a));
+  } 
+  else {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  }
   return arr;
-}
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
 }
